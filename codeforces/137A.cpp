2b65af159aac5d3842b0b7a0cc359b40c59d466b
AKASH VERMA <aakashverma4102@gmail.com>
2022-10-17 21:12:15 +0530

    contest 137

---
+++ codeforces/137A.cpp
@@ -0,0 +1,20 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int k=10-arr.size();
+        int ans=0;
+        for(int i=k;i>0;i--){
+            ans+=((i-1)*6);
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
---
+++ codeforces/137C.cpp
@@ -0,0 +1,40 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int main(){
+    int t;cin>>t;
+    while(t--){
+        int n;cin>>n;
+        string s;cin>>s;
+        vector<int>arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        int ans=0;
+        for(int i=1;i<n;i++){
+            if(s[i]=='1'){
+                if(s[i-1]=='1'){
+                    int j=i;
+                    while(j>=0 and s[j]=='1'){
+                        j--;
+                    }
+                    if(arr[j]>arr[i]){
+                        s[j]='1';
+                        s[i]='0';
+                    }
+                }
+                else{
+                    if(arr[i-1]>arr[i]){
+                        s[i-1]='1';
+                        s[i]='0';
+                    }
+                } 
+            }
+        }
+        for(int i=0;i<n;i++){
+            if(s[i]=='1')ans+=arr[i];
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
